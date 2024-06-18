102e134679098005345913b2a4c00b395812e6f2
Karina <kebets.karina@gmail.com>
2024-06-18 18:53:55 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logotype from "./assets/img/logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sideBar">
+        <img src={logotype} alt="logotype" className="logo"></img>
+        <div className="menu">
+          <div className="navItem1">Movies</div>
+          <div className="navItem2">Rated movies</div>
+        </div>
+      </aside>
+
+      <div className="content">
+        <div className="title">Movies</div>
+      </div>
+
+      <div className="categories">
+        <div className="geners">
+          Geners
+          <select multiply name="genre" className="selectLarge">
+            <summary>Select genre</summary>
+          </select>
+        </div>
+        <div className="year">
+          Release year
+          <select multiply name="year" className="selectLarge">
+            <summary>Year release</summary>
+          </select>
+        </div>
+        <div className="rating">
+          <p className="text">Ratings</p>
+          <select multiply name="ratingFrom" className="selectSmall">
+            <summary>From</summary>
+          </select>
+          <select multiply name="ratingTo" className="selectSmall">
+            <summary>To</summary>
+          </select>
+        </div>
+        <div className="reset">Reset filters</div>
+      </div>
     </div>
   );
 }
