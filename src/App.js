c14d4527450a2dd208fe3fe600f4549569986b36
Karina <kebets.karina@gmail.com>
2024-07-05 21:48:53 +0200

    исправление вёрстки на flex

--- src/App.js
+++ src/App.js
@@ -7,39 +7,64 @@ function App() {
       <aside className="sideBar">
         <img src={logotype} alt="logotype" className="logo"></img>
         <div className="menu">
-          <div className="navItem1">Movies</div>
-          <div className="navItem2">Rated movies</div>
+          <button className="navItem">Movies</button>
+          <button className="navItem">Rated movies</button>
         </div>
       </aside>
 
-      <div className="content">
+      <main className="content">
         <div className="title">Movies</div>
-      </div>
 
-      <div className="categories">
-        <div className="geners">
-          Geners
-          <select multiply name="genre" className="selectLarge">
-            <summary>Select genre</summary>
-          </select>
+        <div className="filter_panel">
+          <div className="filter_item">
+            <p className="filter_item__title">Geners</p>
+            <select multiply name="genre" className="filter_item__select">
+              <summary>Select genre</summary>
+            </select>
+          </div>
+          <div className="filter_item">
+            <p className="filter_item__title">Release year</p>
+            <select multiply name="year" className="filter_item__select">
+              <summary>Year release</summary>
+            </select>
+          </div>
+          <div className="filter_item">
+            <p className="filter_item__title">Ratings</p>
+            <div className="filter_item__select">
+              <select
+                multiply
+                name="ratingFrom"
+                className="filter_item__select"
+              >
+                <summary>From</summary>
+              </select>
+              <select multiply name="ratingTo" className="filter_item__select">
+                <summary>To</summary>
+              </select>
+            </div>
+          </div>
+          <button className="filter_item">Reset filters</button>
+          <div className="filter_item">
+            <p className="filter_item__title">Sort by</p>
+            <div className="filter_item__select">
+              <select name="sortBy" className="filter_item__select">
+                <summary>Sort by</summary>
+              </select>
+            </div>
+          </div>
         </div>
-        <div className="year">
-          Release year
-          <select multiply name="year" className="selectLarge">
-            <summary>Year release</summary>
-          </select>
+        <div className="film_cards">
+          <div className="film_item">
+            <img className="filmImg"></img>
+            <div className="filmSummary">
+              <p className="film_title">The Green Mile</p>
+              <p className="year_relise">1999</p>
+              <div className="rating"></div>
+            </div>
+          </div>
+          <div className="film_item"></div>
         </div>
-        <div className="rating">
-          <p className="text">Ratings</p>
-          <select multiply name="ratingFrom" className="selectSmall">
-            <summary>From</summary>
-          </select>
-          <select multiply name="ratingTo" className="selectSmall">
-            <summary>To</summary>
-          </select>
-        </div>
-        <div className="reset">Reset filters</div>
-      </div>
+      </main>
     </div>
   );
 }
